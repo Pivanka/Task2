3088828b38bd97529c9c67a018e89c533ff96bed
Ivanka <ivanka.polyuhovych@gmail.com>
2023-02-21 10:32:27 +0200

    Add changes for task3

--- BLL/Dtos/BookDto.cs
+++ BLL/Dtos/BookDto.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+        public string Cover { get; set; }
         public decimal Rating { get; set; }
         public decimal ReviewsNumber { get; set; }
     }
